4265db2a8347c25188fe1c49be9511128ad32c6f
H-arish <61192090+H-arish@users.noreply.github.com>
2020-03-15 23:16:52 +0530

    Add files via upload

---
+++ edit.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DBserviceService } from '../dbservice.service';
+
+
+@Component({
+  selector: 'app-edit',
+  templateUrl: './edit.component.html',
+  styleUrls: ['./edit.component.css']
+})
+export class EditComponent implements OnInit {
+  angForm: FormGroup;
+  p: any = {};
+
+  constructor(private route: ActivatedRoute, private router: Router, public dbservice:DBserviceService, private fb: FormBuilder)
+   { this.createForm();
+  }
+  
+
+  ngOnInit() {
+  }
+  createForm() {
+    this.angForm = this.fb.group({
+      EmpID: ['', Validators.required ],
+      EmpName: ['', Validators.required ],
+      Email: ['', Validators.required ],
+      Phone: ['', Validators.required ]
+
+    });
+  }
+
+}
+
